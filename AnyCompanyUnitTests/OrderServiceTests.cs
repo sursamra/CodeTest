95246d43408140339e853cd5eca6b1ca3e3629ac
AH0669059 <AH0669059@EGBLHCND6476R60>
2018-12-01 12:17:03 +0000

    More tests

--- AnyCompanyUnitTests/OrderServiceTests.cs
+++ AnyCompanyUnitTests/OrderServiceTests.cs
@@ -20,7 +20,6 @@ namespace AnyCompanyUnitTests
 
             var orderService = new OrderService(orderRepository.Object, customerRepository.Object);
             var customerService = new CustomerService(customerRepository.Object, orderRepository.Object);
-
                                 
             var customers = new List<Customer>
             {
@@ -77,7 +76,6 @@ namespace AnyCompanyUnitTests
             var orderService = new OrderService(orderRepository.Object, customerRepository.Object);
             var customerService = new CustomerService(customerRepository.Object, orderRepository.Object);
 
-
             var customers = new List<Customer>
             {
                 new Customer()
@@ -113,14 +111,62 @@ namespace AnyCompanyUnitTests
 
             var customersWithOrders = customerService.LoadCustomerWithLinkedOrders();
 
-            //Verify no order returned for John.           
-            Assert.IsTrue(customersWithOrders.First(x => x.Customer.Name == "John").Orders.Count() == 0);
-
+            Assert.IsTrue(success);
             //Verify order returned belongs to Tina.
             Assert.IsTrue(customersWithOrders.First(x => x.Customer.Name == "Tina").Orders.Count() == 1
                             && customersWithOrders.First(x => x.Customer.Name == "Tina").Orders.First().VAT ==0 );
 
             Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void TestUKVAT()
+        {
+            var orderRepository = new Mock<IOrderRepository>();
+            var customerRepository = new Mock<ICustomerRepository>();
+
+            var orderService = new OrderService(orderRepository.Object, customerRepository.Object);
+            var customerService = new CustomerService(customerRepository.Object, orderRepository.Object);
+            
+            var customers = new List<Customer>
+            {
+                new Customer()
+                {
+                    ID = 1,Country = "UK",DateOfBirth = DateTime.Now.AddYears(-50),Name = "John"
+                },
+                new Customer()
+                {
+                    ID = 2,Country = "GE",DateOfBirth = DateTime.Now.AddYears(-40),Name = "Tina"
+                }
+            };
+
+            // Let create an order that is not placed yet
+            Order order = new Order()
+            {
+                Amount = 500
+            };
+            var orders = new List<Order>()
+            {
+              order
+            };
+
+            // lets order it for John
+            Customer c = customers.Find(x => x.Name == "John");
+
+            orderRepository.Setup(r => r.Save(order)).Callback(() => order.Customer = c);
+            customerRepository.Setup(r => r.Load(c.ID)).Returns(c);
+
+            var success = orderService.PlaceOrder(order, 1);
+
+            orderRepository.Setup(r => r.GetCustomerOrders(c)).Returns(orders);
+            customerRepository.Setup(r => r.GetAllCustomers()).Returns(customers);
+
+            var customersWithOrders = customerService.LoadCustomerWithLinkedOrders();
+
+            Assert.IsTrue(success);
+            //Verify order returned belongs to John.
+            Assert.IsTrue(customersWithOrders.First(x => x.Customer.Name == "John").Orders.Count() == 1
+                            && customersWithOrders.First(x => x.Customer.Name == "John").Orders.First().VAT == 0.2);                     
 
         }
         [TestMethod]
